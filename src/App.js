7cf9a8701210caacf1ee1af0cac1767298cfb7d0
Void787 <1195841@student.roc-nijmegen.nl>
2023-04-26 14:02:45 +0200

    voor het eerst de resultaten op depagina gekregen nu moet ik nog de router doen en een css framework erin zetten.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
+import React, { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [weatherData, setWeatherData] = useState({});
+
+  useEffect(() => {
+    const apikey = "9c4e5bebac3febfd0a55eb29f150b2ec"; // replace with your OpenWeather API key
+    const city = "Nijmegen"; // replace with the city you want to display the weather for
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`)
+      .then(response => response.json())
+      .then(data => {
+        setWeatherData({
+          weather: data.weather[0].description,
+          temperature: data.main.temp,
+          feelsLike: data.main.feels_like,
+          humidity: data.main.humidity,
+        });
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+  const { weather, temperature, feelsLike, humidity } = weatherData;
+  const weather_use = `The weather in Nijmegen is currently ${weather}.`;
+  const temperature_use = `The temperature is ${temperature}°C, but it feels like ${feelsLike}°C.`;
+  const humidity_use = `The humidity is ${humidity}%.`;
+  const list = "<li>" + weather_use + "</li> <li>" + temperature_use + "</li> <li>" + humidity_use + "</li>";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body>
+      <header>
+        <h1>My Website</h1>
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
       </header>
-    </div>
+      <main>
+        <section>
+          <h2>About Us</h2>
+          <p></p>
+        </section>
+        <section>
+          <h2>Our Services</h2>
+          <ul id="weather" dangerouslySetInnerHTML={{ __html: list }}></ul>
+        </section>
+      </main>
+      <footer>
+        <p>Copyright © My Website 2023</p>
+      </footer>
+    </body>
   );
 }
 
